5a5a1fbfcfecc96eac2ab1a14d7aae555dd02f19
Lokesh Kose <lokesh.kose@gmail.com>
2024-02-07 12:13:58 +0530

    Removed: import of use effect

--- src/common/Permission/RoutePermission.tsx
+++ src/common/Permission/RoutePermission.tsx
@@ -1,5 +1,5 @@
 // withRBAC.tsx
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getLoggedInUser } from '../../utils/common';
 

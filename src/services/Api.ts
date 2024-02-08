3cc4c5fc140f0e78898affa669ec28bf06b59d8f
Lokesh Kose <lokesh.kose@gmail.com>
2024-02-08 09:36:04 +0530

    Reverted: Base url changes

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosInstance } from 'axios';
 import { message } from 'antd';
 import { NavigateFunction } from 'react-router-dom';
-const BASE_URL = 'https://order.tradekings.app:4001/dg/testing/api/';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 let navigate: NavigateFunction | null = null;
 
 export const setNavigate = (nav: NavigateFunction) => {
